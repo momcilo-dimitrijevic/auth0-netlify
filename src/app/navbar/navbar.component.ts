835091e73077ce1a58726665a50dd6ea881a62ea
momdimit <momcilo.dimitrijevic@inmeta.no>
2020-03-08 13:50:52 +0100

    CSS fix

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from "../auth.service";
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.css"]
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
   constructor(public auth: AuthService) {}
